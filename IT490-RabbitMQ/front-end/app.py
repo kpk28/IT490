e254a0e7403fca769f32148206c715244c7b546a
Shawn Aranda <sca29@njit.edu>
2021-03-19 19:39:53 -0400

    Added error handling for player not found, user shown on results at the top

--- IT490-RabbitMQ/front-end/app.py
+++ IT490-RabbitMQ/front-end/app.py
@@ -78,9 +78,9 @@ def login():
     return render_template('login.html')
 # end::login[] 
 
-# Experimenting =3
+# Get Player Data
 def requestPlayerData(region, player, apikey):
-    # This is my code; Nobody elses ;3
+    # Tailored URL using given input
     URL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + player + "?api_key=" + apikey
     print(URL)
     #requests.get is a function given to us my our import "requests". It basically goes to the URL we made and gives us back a JSON.
@@ -88,8 +88,9 @@ def requestPlayerData(region, player, apikey):
     #Here I return the JSON we just got.
     return response.json()
     
-# This is my code ;3
+# Get Ranked Data
 def requestRankedData(region, ID, APIKey):
+    # Tailored URL using given input
     URL = "https://" + region + ".api.riotgames.com/lol/league/v4/entries/by-summoner/" + ID + "?api_key=" + APIKey
     print(URL)
     print('\n')
@@ -98,22 +99,21 @@ def requestRankedData(region, ID, APIKey):
 
 # Get Spectator Data (Players that are currently in a game)    
 def requestSpectatorData(region, ID, APIKey):
+    # Tailored URL using given input
     URL = "https://" + region + ".api.riotgames.com/lol/spectator/v4/active-games/by-summoner/" + ID + "?api_key=" + APIKey
     print(URL)
     print('/n')
     response = requests.get(URL)
     return response.json()
     
-
 # Requesting from Data Dragon
 def champ_name(champ_id, dictionary):
         for item in list(dictionary["data"].keys()):
                 if int(dictionary["data"][item]["key"]) == int(champ_id):
                         return item
-        return "champion does not exist"
-# Experimenting =3
+        return "champion does not exist"    
     
-# $tag::index[]
+# $tag::index1[]
 @app.route('/', methods=['GET', 'POST'])
 def index1():
     if request.method == 'POST':
@@ -123,10 +123,20 @@ def index1():
         player = request.form['player']
         apikey = request.form['apikey']
         playerDataURL = requestPlayerData(region, player, apikey)
+        
+        #  If the player doesn't exist, return error
+        try:
+            if playerDataURL['status']['status_code'] == 404:
+                return render_template('/playerResults.html',
+                    playerDataError = playerDataURL['status']['message'])    
+        except:
+            print("wub")
+        
         ID = playerDataURL['id']
         accountId = playerDataURL['accountId']
         puuid = playerDataURL['puuid']
         summonerLevel = playerDataURL['summonerLevel']
+        playerName = playerDataURL['name']
         #/End Player Data
         
         # Get Ranked Data (League V4)
@@ -166,12 +176,12 @@ def index1():
                 gameLength = str(datetime.timedelta(seconds=spectatorData['gameLength']+150))
                 gameMode = spectatorData['gameMode']
                 gameMap = ""
-                  
+
                 if(teamId == 100):
                     blueTeam[summonerName] = championName
                 else:
                     redTeam[summonerName] = championName
-            
+
                 if(gameMode == "CLASSIC"):
                     gameMap = "Game Map : " + "Summoner's Rift"
                 else:
@@ -179,6 +189,7 @@ def index1():
         #/End Spectator Data
             
         # Send everything to playerResults.html
+        
         #  If there is a player, they are Ranked, BUT they are not currently in a game
         if playerDataResponseCode == 200 and rankedDataResponseCode == 200 and spectatorDataResponseCode == 404:
             return render_template("playerResults.html", 
@@ -189,6 +200,7 @@ def index1():
                 tr = "Tier Rank : " + tier, 
                 rk = rank, 
                 lp = "LP : " + str(leaguePoints),
+                pN = playerName,
                 spectatorError = "Player is not currently in a game: Error " + str(spectatorDataResponseCode))
         #  If there is a player, they are Ranked, they are currently in a game
         elif playerDataResponseCode == 200 and rankedDataResponseCode == 200 and spectatorDataResponseCode == 200:
@@ -201,10 +213,11 @@ def index1():
                 rk = rank, 
                 lp = "LP : " + str(leaguePoints),
                 sN = "Summoner Name : " + summonerName,
-                gL = "Current Game Time : ~" + gameLength,
-                gM = gameMap,
+                gL = "Current Game Time : " + gameLength,
+                gM = "Map : " + gameMap,
                 rT = "Red Team : " + str(redTeam),
-                bT = "Blue Team : " + str(blueTeam))
+                bT = "Blue Team : " + str(blueTeam),
+                pN = playerName)
         #  If there is a player, they are NOT Ranked, they are currently in a game
         elif playerDataResponseCode == 200 and rankedDataResponseCode == 0 and spectatorDataResponseCode == 200:
             return render_template("playerResults.html", 
@@ -214,21 +227,22 @@ def index1():
                 level = "Summoner Level : " + str(summonerLevel),
                 rankedError = "Player is not ranked : Error "+ str(rankedDataResponseCode),
                 sN = "Summoner Name : " + summonerName,
-                gL = "Current Game Time : ~" + gameLength,
-                gM = gameMap,
+                gL = "Current Game Time : " + gameLength,
+                gM = "Map : " + gameMap,
                 rT = "Red Team : " + str(redTeam),
-                bT = "Blue Team : " + str(blueTeam))
+                bT = "Blue Team : " + str(blueTeam),
+                pN = playerName)
         #  If there is a player, they are NOT Ranked, they are NOT currently in a game
         elif playerDataResponseCode == 200 and rankedDataResponseCode == 0 and spectatorDataResponseCode == 404:
             return render_template("playerResults.html", 
                 pid = "Player ID : " + ID,
+                pN = playerName,
                 acctID = "Account ID : " + accountId,
                 puid = "Player Universely Unique Identifier : " + puuid,
                 level = "Summoner Level : " + str(summonerLevel),
-                rankedError = "Player is not in ranked : Error " + str(rankedDataResponseCode),
+                rankedError = "Player is not ranked : Error " + str(rankedDataResponseCode),
                 spectatorError = "Player is not currently in a game: Error " + str(spectatorDataResponseCode))               
-# end::index[]
-
+# end::index1[]
 
 @app.route('/logout')
 def logout():
